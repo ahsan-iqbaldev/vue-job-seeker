c816a2316d90af2cff94e21e906bcbf7207ca2e8
Ahsan Iqbal <revensolutions01@gmail.com>
2024-07-07 23:02:14 +0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+import Navbar from "./components/Navbar.vue";
+import Hero from "./components/Hero.vue";
+</script>
+<template>
+  <Navbar />
+  <Hero title="Become a Vue Dev" subTitle="Find the Vue job that fits your skills and needs" />
+</template>
