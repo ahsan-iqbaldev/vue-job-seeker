c153815f78e36a8eed0667c211ba6518238b5b69
Ahsan Iqbal <revensolutions01@gmail.com>
2024-07-07 23:17:56 +0500

    create Dynamic home cards

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <script setup>
 import Navbar from "./components/Navbar.vue";
 import Hero from "./components/Hero.vue";
+import HomeCards from "./components/HomeCards.vue";
 </script>
 <template>
   <Navbar />
-  <Hero title="Become a Vue Dev" subTitle="Find the Vue job that fits your skills and needs" />
+  <Hero
+    title="Become a Vue Dev"
+    subTitle="Find the Vue job that fits your skills and needs"
+  />
+  <HomeCards />
 </template>
